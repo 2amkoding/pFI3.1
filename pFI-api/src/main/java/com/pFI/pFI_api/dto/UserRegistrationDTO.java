6654d30833cfe82650fcf1a06db6c72504c9ae01
2amkoding <2amkoding@gmail.com>
2025-02-25 11:24:05 -0600

    [backend] Update - DTO annotations and PW Policy annotations

--- pFI-api/src/main/java/com/pFI/pFI_api/dto/UserRegistrationDTO.java
+++ pFI-api/src/main/java/com/pFI/pFI_api/dto/UserRegistrationDTO.java
@@ -1,20 +1,30 @@
 package com.pFI.pFI_api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.processing.Pattern;
 import org.springframework.http.converter.json.GsonBuilderUtils;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRegistrationDTO {
-    private Long id;
+    @NotBlank(message = "first name is required")
     private String firstName;
+
+    @NotBlank(message = "last name is required")
     private String lastName;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "username is required")
+    @Size(min = 4, max = 50)
     private String username;
+
+    @NotBlank(message = "password is required")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                message = "password must be at least 8 characters and contain both letters and numbers"  )
     private String password;
 }
