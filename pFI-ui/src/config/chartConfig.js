502b6777f9e43b812083ea2c944cf50673f594ff
2amkoding <2amkoding@gmail.com>
2025-04-03 11:53:25 -0500

    [frontEnd] CREATE - ChartConfig

---
+++ pFI-ui/src/config/chartConfig.js
@@ -0,0 +1,13 @@
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+};
+
+export default chartConfig;
+
